3cbdcc7a60d851e742c97448a29e5bd58759deab
HamxaJ <hamza.javed@empglabs.com>
2021-07-15 16:07:15 +0500

    added form request for creating and updating record in patient table

---
+++ app/Http/Requests/PatientRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PatientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required' ,
+            'last_name' => 'required',
+            'email' => 'required|email|unique:patients',
+            'age' => 'required|numeric',
+            'contact_number' => 'required|regex:/(03)[0-9]{9}/',
+            'symptoms' => 'sometimes|required',
+            'first_symptom_date' => 'sometimes|required|date',
+            'is_tested' => 'required|boolean',
+            'test_date' => 'sometimes|required|date',
+            'is_recovered' => 'required|boolean',
+            'recovery_date' => 'sometimes|required|date',
+        ];
+    }
+}
