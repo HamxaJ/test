a2a3c5afe5eeb672f1f9a95c35bcbf1d78653cb8
HamxaJ <hamza.javed@empglabs.com>
2021-07-15 13:11:26 +0500

    Added migration for patients table and created resource controller

---
+++ app/Models/Patient.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    use HasFactory;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'age',
+        'contact_number',
+        'symptoms',
+        'first_symptom_date',
+        'is_tested',
+        'test_date',
+        'is_recovered',
+        'recovery_date',
+        'updated_at',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'symptoms' => 'array',
+    ];
+
+}
---
+++ database/migrations/2021_07_15_055223_create_patients_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->integer('age');
+            $table->integer('contact_number');
+            $table->string('symptoms');
+            $table->date('first_symptom_date');
+            $table->boolean('is_tested');
+            $table->date('test_date')->nullable();
+            $table->boolean('is_recovered');
+            $table->date('recovery_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
